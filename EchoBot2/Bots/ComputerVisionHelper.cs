141fa095d5222dfd3a573440db2938ef4b44b072
twdeveloper <twdeveloper@outlook.com>
2021-06-04 00:21:20 +0800

    v1

---
+++ EchoBot2/Bots/ComputerVisionHelper.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+
+namespace EchoBot2.Bots
+{
+
+
+    public class ComputerVisionHelper
+    {
+
+        public static dynamic MakeRequest(string endpoint, string subscriptionKey, byte[] byteData)
+        {
+            HttpClient client = new HttpClient();
+            string uriBase = endpoint + "vision/v2.1/analyze";
+
+            // Request headers.
+            client.DefaultRequestHeaders.Add(
+                "Ocp-Apim-Subscription-Key", subscriptionKey);
+
+            string requestParameters =
+                "visualFeatures=Categories,Description,Color";
+
+            // Assemble the URI for the REST API method.
+            string uri = uriBase + "?" + requestParameters;
+
+            HttpResponseMessage response;
+
+            // Add the byte array as an octet stream to the request body.
+            using (ByteArrayContent content = new ByteArrayContent(byteData))
+            {
+                // This example uses the "application/octet-stream" content type.
+                // The other content types you can use are "application/json"
+                // and "multipart/form-data".
+                content.Headers.ContentType =
+                    new System.Net.Http.Headers.MediaTypeHeaderValue("application/octet-stream");
+
+                // Asynchronously call the REST API method.
+                response = client.PostAsync(uri, content).Result;
+            }
+
+            // Asynchronously get the JSON response.
+            string JSON = response.Content.ReadAsStringAsync().Result;
+
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(JSON);
+        }
+
+        /// <summary>
+        /// 處理照片
+        /// </summary>
+        /// <param name="LineEvent"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        public static dynamic ProcessImage(string endpoint, string subscriptionKey, byte[] byteData)
+        {
+            string Msg = "";
+            string analyze = "";
+
+            System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls12;
+
+            //取得圖片檔案FileStream, 分別作為繪圖與分析用
+            Stream MemStream1 = new MemoryStream(byteData);
+            Stream MemStream2 = new MemoryStream(byteData);
+            //繪圖用
+            System.Drawing.Bitmap bmp = new System.Drawing.Bitmap(MemStream1);
+            System.Drawing.Graphics g = System.Drawing.Graphics.FromImage(bmp);
+            //ComputerVision instance
+            var visionClient = new Microsoft.Azure.CognitiveServices.Vision.ComputerVision.ComputerVisionClient(
+                 new Microsoft.Azure.CognitiveServices.Vision.ComputerVision.ApiKeyServiceClientCredentials(subscriptionKey))
+            { Endpoint = endpoint };
+
+            //分析用
+            using (MemStream2)
+            {
+                var features = new List<VisualFeatureTypes?>() { VisualFeatureTypes.Faces, VisualFeatureTypes.Description };
+                //分析圖片
+                var Results = visionClient.AnalyzeImageInStreamAsync(
+                    MemStream2, features).Result;
+                //分別保存性別數量
+                int isM = 0, isF = 0;
+                //如果找到臉，就畫方框標示出來
+                foreach (var item in Results.Faces)
+                {
+                    var faceRect = item.FaceRectangle;
+                    //畫框
+                    g.DrawRectangle(
+                                new Pen(Brushes.Red, 3),
+                                new Rectangle(faceRect.Left, faceRect.Top,
+                                    faceRect.Width, faceRect.Height));
+                    //在方框旁邊顯示年紀
+                    var age = 0;
+                    if (item.Gender == Gender.Female) age = item.Age - 2; else age = item.Age;
+                    //劃出數字
+                    g.DrawString(age.ToString(), new Font(SystemFonts.DefaultFont.FontFamily, 24, FontStyle.Bold),
+                        new SolidBrush(Color.Black),
+                        faceRect.Left + 3, faceRect.Top + 3);
+                    //紀錄性別數量
+                    if (item.Gender == Gender.Male)
+                        isM += 1;
+                    else
+                        isF += 1;
+                }
+                //圖片分析結果
+                Msg += $"你上傳的圖片，經過辨識是：";
+                try
+                {
+                    analyze = Results.Description.Captions[0].Text;
+                    Msg += $"\n{ analyze}";
+                }
+                catch (Exception ex)
+                {
+                    Msg += $"\n{ Results.Description.Captions[0].Text}";
+                }
+
+
+                //如果update了照片，則顯示新圖
+                if (Results.Faces.Count() > 0)
+                {
+                    Msg += String.Format("\n\n找到{0}張臉, \n{1}男 {2}女", Results.Faces.Count(), isM, isF);
+                }
+                else
+                {
+                    Msg += "\n\n這照片裡面沒有人耶...";
+                }
+            }
+
+            string ImgurURL = "";
+            using (MemoryStream m = new MemoryStream())
+            {
+                bmp.Save(m, System.Drawing.Imaging.ImageFormat.Png);
+                ImgurURL = UploadImage2Imgur(m.ToArray());
+            }
+
+            dynamic ret = new { Text = Msg, ImageURL = new Uri(ImgurURL) };
+            //一次把集合中的多則訊息回覆給用戶
+            return ret;
+        }
+
+        //Upload Image to Imgur
+        private static string UploadImage2Imgur(byte[] bytes)
+        {
+            //建立 ImgurClient準備上傳圖片
+            var client = new Imgur.API.Authentication.Impl.ImgurClient("___06cb4ff2___", "_____054a2c25c99786bc265f3f25455aa_____");
+            var endpoint = new Imgur.API.Endpoints.Impl.ImageEndpoint(client);
+            Imgur.API.Models.IImage image;
+            //上傳Imgur
+            image = endpoint.UploadImageStreamAsync(new MemoryStream(bytes)).GetAwaiter().GetResult();
+            return image.Link;
+        }
+
+    }
+
+}
---
+++ EchoBot2/Bots/EchoBot.cs
@@ -0,0 +1,61 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.13.2
+
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+
+namespace EchoBot2.Bots
+{
+    public class EchoBot : ActivityHandler
+    {
+        static string endpoint = "https://southeastasia.api.cognitive.microsoft.com/";
+        static string subscriptionKey = "______5240bab67f2cb________";
+
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var replyText = $"Echo : {turnContext.Activity.Text}";
+
+            //if (turnContext.Activity.Attachments.Count > 0)
+            //{
+            //    var attachmentUrl = turnContext.Activity.Attachments[0].ContentUrl;
+            //    var httpClient = new HttpClient();
+            //    var attachmentData =
+            //        await httpClient.GetByteArrayAsync(attachmentUrl);
+
+            //    var ret = ComputerVisionHelper.MakeRequest(endpoint, subscriptionKey, attachmentData);
+            //     replyText = $"圖片內容是: {ret.description.captions[0]}";
+            //}
+
+            if (turnContext.Activity.Attachments.Count > 0)
+            {
+                var attachmentUrl = turnContext.Activity.Attachments[0].ContentUrl;
+                var httpClient = new HttpClient();
+                var attachmentData =
+                    await httpClient.GetByteArrayAsync(attachmentUrl);
+
+                var ret = ComputerVisionHelper.ProcessImage(endpoint, subscriptionKey, attachmentData);
+                replyText = $"圖片內容是 -->  {ret.Text}  {System.Environment.NewLine} URL: {ret.ImageURL}";
+            }
+
+            await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var welcomeText = "Hello and welcome!";
+            foreach (var member in membersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
+                }
+            }
+        }
+    }
+}
